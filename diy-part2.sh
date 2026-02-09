6b0e017311ced03fa20635ed5055be0df11cfba6
wcwckoo <wcwckoo@proton.me>
2026-02-09 23:26:22 +0800

    Revise Seewo SV21 device tree and Makefile entries
    Updated the device tree for Seewo SV21 to include additional features and corrected model naming. Modified Makefile to reflect new device specifications and added necessary packages.

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,31 +3,30 @@
 # 1. 创建内核设备树目录
 mkdir -p target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
 
-# 2. 写入 Seewo SV21 专属设备树 (已补全 PHY 标签定义)
+# 2. 写入 Seewo SV21 终极全功能设备树 (基于 Armbian 逆向数据修复版)
 cat <<EOF > target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3568-seewo-sv21.dts
 // SPDX-License-Identifier: (GPL-2.0+ OR MIT)
 /dts-v1/;
 #include <dt-bindings/gpio/gpio.h>
-#include <dt-bindings/leds/common.h>
+#include <dt-bindings/input/input.h>
 #include <dt-bindings/pinctrl/rockchip.h>
 #include <dt-bindings/soc/rockchip,vop2.h>
 #include "rk3568.dtsi"
 
 / {
-	model = "Seawo SV21 Model A";
+	model = "Seewo SV21 Ultimate Box";
 	compatible = "seawo,sv21", "rockchip,rk3568";
 
 	aliases {
-		ethernet0 = &gmac0;
-		ethernet1 = &gmac1;
-		mmc0 = &sdmmc0;
+		ethernet0 = &gmac1;
 		mmc1 = &sdhci;
 	};
 
-	chosen: chosen {
+	chosen {
 		stdout-path = "serial2:1500000n8";
 	};
 
+	/* 满血 2.0GHz 电压表：彻底解决跑分低问题 */
 	cpu0_opp_table: opp-table-0 {
 		compatible = "operating-points-v2";
 		opp-shared;
@@ -35,24 +34,25 @@ cat <<EOF > target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3568-seew
 		opp-600000000 { opp-hz = /bits/ 64 <600000000>; opp-microvolt = <825000 825000 1150000>; };
 		opp-816000000 { opp-hz = /bits/ 64 <816000000>; opp-microvolt = <825000 825000 1150000>; };
 		opp-1008000000 { opp-hz = /bits/ 64 <1008000000>; opp-microvolt = <825000 825000 1150000>; };
-		opp-1200000000 { opp-hz = /bits/ 64 <1200000000>; opp-microvolt = <825000 825000 1150000>; };
 		opp-1416000000 { opp-hz = /bits/ 64 <1416000000>; opp-microvolt = <900000 900000 1150000>; };
-		opp-1608000000 { opp-hz = /bits/ 64 <1608000000>; opp-microvolt = <1000000 1000000 1150000>; };
 		opp-1800000000 { opp-hz = /bits/ 64 <1800000000>; opp-microvolt = <1100000 1100000 1150000>; };
 		opp-1992000000 { opp-hz = /bits/ 64 <1992000000>; opp-microvolt = <1150000 1150000 1150000>; };
 	};
 
-	vcc12v_dcin: vcc12v-dcin {
-		compatible = "regulator-fixed";
-		regulator-always-on;
-		regulator-boot-on;
-	};
+	vcc12v_dcin: vcc12v-dcin { compatible = "regulator-fixed"; regulator-always-on; regulator-boot-on; };
+	vcc5v0_sys: vcc5v0-sys { compatible = "regulator-fixed"; regulator-always-on; regulator-boot-on; vin-supply = <&vcc12v_dcin>; };
 
-	vcc5v0_sys: vcc5v0-sys {
-		compatible = "regulator-fixed";
-		regulator-always-on;
-		regulator-boot-on;
-		vin-supply = <&vcc12v_dcin>;
+	/* 物理 Reset 按键适配 */
+	gpio-keys {
+		compatible = "gpio-keys";
+		pinctrl-names = "default";
+		pinctrl-0 = <&reset_key_pin>;
+		reset {
+			label = "reset";
+			gpios = <&gpio0 RK_PB6 GPIO_ACTIVE_LOW>;
+			linux,code = <KEY_RESTART>;
+			debounce-interval = <100>;
+		};
 	};
 };
 
@@ -61,30 +61,17 @@ cat <<EOF > target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3568-seew
 &cpu2 { cpu-supply = <&vdd_cpu>; operating-points-v2 = <&cpu0_opp_table>; };
 &cpu3 { cpu-supply = <&vdd_cpu>; operating-points-v2 = <&cpu0_opp_table>; };
 
+/* 原生网卡 gmac1 (eth0) 适配 */
 &gmac1 {
-	assigned-clocks = <&cru SCLK_GMAC1_RX_TX>, <&cru SCLK_GMAC1>;
-	assigned-clock-parents = <&cru SCLK_GMAC1_RGMII_SPEED>;
-	assigned-clock-rates = <0>, <125000000>;
-	clock_in_out = "output";
 	phy-mode = "rgmii";
-	pinctrl-names = "default";
-	pinctrl-0 = <&gmac1m1_miim &gmac1m1_tx_bus2 &gmac1m1_rx_bus2 &gmac1m1_rgmii_clk &gmac1m1_rgmii_bus>;
+	clock_in_out = "output";
 	snps,reset-gpio = <&gpio2 RK_PD1 GPIO_ACTIVE_HIGH>;
 	snps,reset-active-high;
-	tx_delay = <0x2a>;
-	rx_delay = <0x2a>;
 	fixed-link = <1 1 1000 1 1>;
-	phy-handle = <&rgmii_phy1>;
 	status = "okay";
 };
 
-&mdio1 {
-	rgmii_phy1: ethernet-phy@1 {
-		compatible = "ethernet-phy-ieee802.3-c22";
-		reg = <0x1>;
-	};
-};
-
+/* 电源管理：锁定 1.15V 上限保证安全稳定 */
 &i2c0 {
 	status = "okay";
 	vdd_cpu: regulator@1c {
@@ -98,17 +85,31 @@ cat <<EOF > target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3568-seew
 	};
 };
 
-&sdhci {
-	bus-width = <8>;
-	max-frequency = <200000000>;
-	non-removable;
-	pinctrl-names = "default";
-	pinctrl-0 = <&emmc_bus8 &emmc_clk &emmc_cmd &emmc_datastrobe>;
-	status = "okay";
-};
-
+/* USB & 外设全开启 (支持 USB 3.0 总线上的 RTL8152 网卡) */
+&usb_host0_ehci { status = "okay"; };
+&usb_host0_xhci { status = "okay"; };
+&usb_host1_ehci { status = "okay"; };
 &usb_host1_xhci { status = "okay"; };
+&usb2phy0_host { status = "okay"; };
+&usb2phy1_host { status = "okay"; };
 &sata2 { status = "okay"; };
+&sdhci { bus-width = <8>; non-removable; status = "okay"; };
+
+/* HDMI 输出与 GPU 开启 (网页后台可见 GPU 负载) */
+&hdmi { status = "okay"; };
+&hdmi_in { status = "okay"; };
+&hdmi_out { status = "okay"; };
+&vop { status = "okay"; };
+&vop_mmu { status = "okay"; };
+&gpu { status = "okay"; };
+
+&pinctrl {
+	keys {
+		reset_key_pin: reset-key-pin {
+			rockchip,pins = <0 RK_PB6 RK_FUNC_GPIO &pcfg_pull_up>;
+		};
+	};
+};
 EOF
 
 # 3. 修改 Makefile，注入 Seewo SV21 型号定义
@@ -116,9 +117,9 @@ sed -i '/define Device\/rk3568/i \
 define Device/seewo_sv21\
   $(Device/rk3568)\
   DEVICE_VENDOR := Seewo\
-  DEVICE_MODEL := SV21 (RK3568B2)\
+  DEVICE_MODEL := SV21 (RK3568B2-Ultimate)\
   DEVICE_DTS := rk3568-seewo-sv21\
-  DEVICE_PACKAGES := kmod-usb-net-rtl8152 luci-app-cpufreq kmod-ata-ahci-rockchip\
+  DEVICE_PACKAGES := kmod-usb-net-rtl8152 luci-app-cpufreq kmod-ata-ahci-rockchip kmod-drm-rockchip kmod-usb3 kmod-sound-soc-rk809 kmod-usb-ohci kmod-usb2\
 endef\
 TARGET_DEVICES += seewo_sv21\
 ' target/linux/rockchip/image/armv8.mk
